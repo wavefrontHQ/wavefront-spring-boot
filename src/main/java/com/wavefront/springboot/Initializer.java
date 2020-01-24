d55bd0b956b828f6f458ea011a3ea06c3e87985b
Clement Pang <clement@wavefront.com>
2020-01-23 17:18:36 -0800

    Add project configuration files

--- src/main/java/com/wavefront/springboot/Initializer.java
+++ src/main/java/com/wavefront/springboot/Initializer.java
@@ -22,7 +22,6 @@ import io.opentracing.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationContext;
