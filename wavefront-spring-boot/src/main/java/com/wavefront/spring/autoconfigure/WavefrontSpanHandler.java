62f526a2098c574d9c853bd1e007ec7797afb09f
Adrian Cole <acole@pivotal.io>
2020-04-23 15:23:25 +0800

    Stabs integrating RED metrics, though limited by internal apis
    I tried this, but the service seems hung (maybe from my network). Also,
    I had to do some guessing to convert internal apis into Micrometer.
    Finally, we aren't configuring the heartbeater, yet.
    
    Hopefully, someone can pick this up when ready to unravel those things.

--- wavefront-spring-boot/src/main/java/com/wavefront/spring/autoconfigure/WavefrontSpanHandler.java
+++ wavefront-spring-boot/src/main/java/com/wavefront/spring/autoconfigure/WavefrontSpanHandler.java
@@ -9,6 +9,7 @@ import java.util.Locale;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import brave.handler.FinishedSpanHandler;
@@ -21,11 +22,15 @@ import com.wavefront.sdk.entities.tracing.SpanLog;
 import com.wavefront.sdk.entities.tracing.WavefrontTracingSpanSender;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import static com.wavefront.sdk.common.Constants.APPLICATION_TAG_KEY;
 import static com.wavefront.sdk.common.Constants.DEBUG_TAG_KEY;
 import static com.wavefront.sdk.common.Constants.ERROR_TAG_KEY;
+import static com.wavefront.sdk.common.Constants.NULL_TAG_VAL;
+import static com.wavefront.sdk.common.Constants.SERVICE_TAG_KEY;
 import static com.wavefront.sdk.common.Constants.SOURCE_KEY;
 import static com.wavefront.sdk.common.Constants.SPAN_LOG_KEY;
 
@@ -53,29 +58,57 @@ final class WavefrontSpanHandler extends FinishedSpanHandler implements Runnable
   private static final Log LOG = LogFactory.getLog(WavefrontSpanHandler.class);
   // https://github.com/wavefrontHQ/wavefront-proxy/blob/3dd1fa11711a04de2d9d418e2269f0f9fb464f36/proxy/src/main/java/com/wavefront/agent/listeners/tracing/ZipkinPortUnificationHandler.java#L114-L114
   private static final String DEFAULT_SPAN_NAME = "defaultOperation";
+  private static final String INVOCATION_SUFFIX = ".invocation";
+  private static final String ERROR_SUFFIX = ".error";
+  private static final String TOTAL_TIME_SUFFIX = ".total_time.millis";
+  private static final String DURATION_SUFFIX = ".duration.micros";
+  private static final String OPERATION_NAME_TAG = "operationName";
+  private static final String HTTP_STATUS_CODE = "http.status_code";
+  private static final Pattern WHITESPACE = Pattern.compile("[\\s]+");
 
-  final LinkedBlockingQueue<Pair<TraceContext, MutableSpan>> spanBuffer;
+  /** for RED metrics */
+  @Override public boolean alwaysSampleLocal() {
+    return true;
+  }
+
   final WavefrontTracingSpanSender wavefrontSender;
-  final Counter spansDropped;
-  final Counter spansReceived;
-  final Counter reportErrors;
-  final Thread sendingThread;
-
-  private volatile boolean stop = false;
+  final LinkedBlockingQueue<Pair<TraceContext, MutableSpan>> spanBuffer;
 
   final String source;
   final List<Pair<String, String>> defaultTags;
   final Set<String> defaultTagKeys;
 
-  WavefrontSpanHandler(int maxQueueSize, WavefrontTracingSpanSender wavefrontSender, MeterRegistry
-      meterRegistry, String source, List<Pair<String, String>> defaultTags) {
+  final MeterRegistry meterRegistry;
+  final Set<String> redMetricsCustomTagKeys;
+  final String redMetricNamePrefix;
+
+  final Counter spansDropped;
+  final Counter spansReceived;
+  final Counter reportErrors;
+
+  final Thread sendingThread;
+  private volatile boolean stop = false;
+
+  WavefrontSpanHandler(int maxQueueSize, WavefrontTracingSpanSender wavefrontSender,
+      MeterRegistry meterRegistry, String source, List<Pair<String, String>> defaultTags) {
     this.wavefrontSender = wavefrontSender;
+    this.spanBuffer = new LinkedBlockingQueue<>(maxQueueSize);
+
+    this.meterRegistry = meterRegistry;
     this.source = source;
     this.defaultTags = defaultTags;
     this.defaultTagKeys = defaultTags.stream().map(p -> p._1).collect(Collectors.toSet());
     this.defaultTagKeys.add(SOURCE_KEY);
 
-    this.spanBuffer = new LinkedBlockingQueue<>(maxQueueSize);
+    this.redMetricsCustomTagKeys = Collections.emptySet(); // TODO
+    String application = null, service = null;
+    for (Pair<String, String> defaultTag : defaultTags) {
+      if (defaultTag._1.equals(APPLICATION_TAG_KEY)) application = defaultTag._2;
+      if (defaultTag._1.equals(SERVICE_TAG_KEY)) service = defaultTag._2;
+    }
+    // Need to sanitize metric name as application, service and operation names can have spaces
+    // and other invalid metric name characters
+    redMetricNamePrefix = sanitize(application + "." + service + ".");
 
     // init internal metrics
     meterRegistry.gauge("reporter.queue.size", spanBuffer, sb -> (double) sb.size());
@@ -90,9 +123,18 @@ final class WavefrontSpanHandler extends FinishedSpanHandler implements Runnable
     this.sendingThread.start();
   }
 
-  // Exact same behavior as WavefrontSpanReporter
-  // https://github.com/wavefrontHQ/wavefront-opentracing-sdk-java/blob/f1f08d8daf7b692b9b61dcd5bc24ca6befa8e710/src/main/java/com/wavefront/opentracing/reporting/WavefrontSpanReporter.java#L163-L179
+  // Simplified variant of WavefrontSpan, which appears to collect 100% data, but
+  // conditionally sample traces
+  // https://github.com/wavefrontHQ/wavefront-opentracing-sdk-java/blob/dfac3c7c7a730885e021263e7d2e4f9c226ea9e9/src/main/java/com/wavefront/opentracing/WavefrontSpan.java#L245-L269
   @Override public boolean handle(TraceContext context, MutableSpan span) {
+    if (span.finishTimestamp() != 0L && span.startTimestamp() != 0L) {
+      reportWavefrontGeneratedData(context, span);
+    }
+
+    if (!context.sampled()) return true;
+
+    // Below here is the same behavior as WavefrontSpanReporter
+    // https://github.com/wavefrontHQ/wavefront-opentracing-sdk-java/blob/f1f08d8daf7b692b9b61dcd5bc24ca6befa8e710/src/main/java/com/wavefront/opentracing/reporting/WavefrontSpanReporter.java#L163-L179
     spansReceived.increment();
     if (!spanBuffer.offer(Pair.of(context, span))) {
       spansDropped.increment();
@@ -102,6 +144,61 @@ final class WavefrontSpanHandler extends FinishedSpanHandler implements Runnable
       }
     }
     return true; // regardless of error, other handlers should run
+  }
+
+  // https://github.com/wavefrontHQ/wavefront-opentracing-sdk-java/blob/dfac3c7c7a730885e021263e7d2e4f9c226ea9e9/src/main/java/com/wavefront/opentracing/WavefrontTracer.java#L259-L326
+  void reportWavefrontGeneratedData(TraceContext context, MutableSpan span) {
+    // NOTE: We expect the meter registry to already have default tags like cluster, service, etc.
+    List<Tag> pointTags = new ArrayList<>();
+    span.forEachTag((l, k, v) -> {
+      if (redMetricsCustomTagKeys.contains(k)) {
+        l.add(Tag.of(k, v));
+      }
+    }, pointTags);
+
+    String kind = span.kind() != null ? span.kind().name() : NULL_TAG_VAL;
+    pointTags.add(Tag.of("span.kind", kind));
+
+    boolean isError = span.error() != null || span.tag("error") != null;
+    String httpStatus = span.tag(HTTP_STATUS_CODE);
+    Tag httpStatusTag = httpStatus != null ? Tag.of(HTTP_STATUS_CODE, httpStatus) : null;
+    // Propagate HTTP status if the span has error
+    if (httpStatusTag != null && isError) {
+      pointTags.add(httpStatusTag);
+    }
+
+    // Add operation tag after sending RED heartbeat.
+    pointTags.add(Tag.of(OPERATION_NAME_TAG, span.name()));
+
+    String metricNamePrefix = redMetricNamePrefix + sanitize(span.name());
+    if (isError) {
+      meterRegistry.counter(metricNamePrefix + ERROR_SUFFIX, pointTags).increment();
+    }
+
+    // Remove http error status before sending request and duration metrics
+    if (httpStatusTag != null) pointTags.remove(httpStatusTag);
+    meterRegistry.counter(metricNamePrefix + INVOCATION_SUFFIX, pointTags).increment();
+
+    long spanDurationMicros = span.finishTimestamp() - span.startTimestamp();
+    // Convert from micros to millis and add to duration counter
+    meterRegistry.counter(metricNamePrefix + TOTAL_TIME_SUFFIX, pointTags)
+        .increment((double) spanDurationMicros / 1000L);
+
+    if (isError) pointTags.add(Tag.of(ERROR_TAG_KEY, "true"));
+    // Support duration in microseconds instead of milliseconds
+    meterRegistry.summary(metricNamePrefix + DURATION_SUFFIX, pointTags)
+        .record(spanDurationMicros);
+  }
+
+  // https://github.com/wavefrontHQ/wavefront-opentracing-sdk-java/blob/dfac3c7c7a730885e021263e7d2e4f9c226ea9e9/src/main/java/com/wavefront/opentracing/WavefrontTracer.java#L334-L342
+  static String sanitize(String s) {
+    final String whitespaceSanitized = WHITESPACE.matcher(s).replaceAll("-");
+    if (s.contains("\"") || s.contains("'")) {
+      // for single quotes, once we are double-quoted, single quotes can exist happily inside it.
+      return whitespaceSanitized.replaceAll("\"", "\\\\\"");
+    } else {
+      return whitespaceSanitized;
+    }
   }
 
   private void send(TraceContext context, MutableSpan span) {
