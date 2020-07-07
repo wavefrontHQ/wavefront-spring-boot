4d3287669aa6a9d80792e88f6155aa16699aa41d
Stephane Nicoll <snicoll@pivotal.io>
2020-07-07 13:18:10 +0200

    Polish "Turn off unnecessary proxying of configuration classes"
    See gh-67

--- wavefront-spring-boot/src/main/java/com/wavefront/spring/actuate/WavefrontEndpointAutoConfiguration.java
+++ wavefront-spring-boot/src/main/java/com/wavefront/spring/actuate/WavefrontEndpointAutoConfiguration.java
@@ -17,6 +17,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -24,6 +25,7 @@ import org.springframework.web.client.RestTemplate;
  *
  * @author Stephane Nicoll
  */
+@Configuration(proxyBeanMethods = false)
 @ConditionalOnClass({ RestTemplate.class, WavefrontConfig.class, ApplicationTags.class })
 @ConditionalOnBean({ RestTemplateBuilder.class, WavefrontConfig.class, ApplicationTags.class })
 @ConditionalOnAvailableEndpoint(endpoint = WavefrontController.class)
