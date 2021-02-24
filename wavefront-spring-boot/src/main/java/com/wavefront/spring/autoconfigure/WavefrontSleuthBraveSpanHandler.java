02bb7b9adc0ae6abbf9bf87760a154cd87d37ba4
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-02-24 08:45:32 +0100

    New sleuth api for wavefront (#105)
    * Refactored the span handler to use Sleuth's API
    
    * Abstracted stuff - will need to move all of Sleuth stuff back to Sleuth
    
    * Bumped version of cloud train
    
    * Polish

---
+++ wavefront-spring-boot/src/main/java/com/wavefront/spring/autoconfigure/WavefrontSleuthBraveSpanHandler.java
@@ -0,0 +1,35 @@
+package com.wavefront.spring.autoconfigure;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import brave.handler.MutableSpan;
+import brave.handler.SpanHandler;
+import brave.propagation.TraceContext;
+
+import org.springframework.cloud.sleuth.brave.bridge.BraveFinishedSpan;
+import org.springframework.cloud.sleuth.brave.bridge.BraveTraceContext;
+
+class WavefrontSleuthBraveSpanHandler extends SpanHandler implements Runnable, Closeable {
+
+  final WavefrontSleuthSpanHandler spanHandler;
+
+  WavefrontSleuthBraveSpanHandler(WavefrontSleuthSpanHandler spanHandler) {
+    this.spanHandler = spanHandler;
+  }
+
+  @Override
+  public boolean end(TraceContext context, MutableSpan span, Cause cause) {
+    return spanHandler.end(BraveTraceContext.fromBrave(context), BraveFinishedSpan.fromBrave(span));
+  }
+
+  @Override
+  public void close() throws IOException {
+    this.spanHandler.close();
+  }
+
+  @Override
+  public void run() {
+    this.spanHandler.run();
+  }
+}
