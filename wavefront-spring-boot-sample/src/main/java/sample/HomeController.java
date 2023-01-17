a8b1c0e3ab6063256ef3e33b6137c1f6265a8996
Glenn Oppegard <goppegard@vmware.com>
2023-01-17 14:34:42 -0700

    Add Span Event to sample app for demo purposes

--- wavefront-spring-boot-sample/src/main/java/sample/HomeController.java
+++ wavefront-spring-boot-sample/src/main/java/sample/HomeController.java
@@ -1,10 +1,20 @@
 package sample;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+
 @RestController
 public class HomeController {
+  private final Logger logger = Logger.getLogger(HomeController.class.getName());
+
+  @Autowired
+  ObservationRegistry observationRegistry;
 
   @GetMapping("/")
   public String home() {
@@ -13,6 +23,13 @@ public class HomeController {
 
   @GetMapping("/oops")
   public void error() {
+    Observation observation = observationRegistry.getCurrentObservation();
+    if (observation != null) {
+      Observation.Event event = Observation.Event.of("synthetic.error", "a synthetic error");
+      observation.event(event);
+    } else {
+      logger.severe("No current observation to attach event!");
+    }
     throw new IllegalStateException("Test exception");
   }
 
